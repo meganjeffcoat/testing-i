5af7bfc21424008f374c7b1b3de301bea7c9e718
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-03-04 15:09:12 -0600

    made some items both weapons and armor

---
+++ items/items.js
@@ -0,0 +1,35 @@
+module.exports = {
+    trident,
+    head,
+    helm,
+    achilles
+}
+
+
+const trident = {
+    name: 'Trident',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 14
+  }
+
+const head = {
+    name: 'Medusa-Head',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 19
+  }
+
+const helm = {
+    name: 'Helm-of-Darkness',
+    type: 'armor',
+    durability: 100,
+    enhancement: 15
+  }
+
+const achilles = {
+    name: 'Armour-of-Achilles',
+    type: 'armor',
+    durability: 100,
+    enhancement: 18
+  }
